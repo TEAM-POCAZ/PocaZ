8f160167cd856086a5f2bead0cf17ed5151d7477
ho2yahh <ho2yahh@gmail.com>
2022-09-05 18:14:28 +0900

    init: react init setting

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+type btnClickEvent = React.MouseEvent<HTMLElement, MouseEvent>;
+
+const Home = () => {
+    const navigate = useNavigate();
+
+    const onClickNextPage = (event: btnClickEvent): void => {
+        event.preventDefault();
+        navigate("/store");
+    };
+
+    return (
+        <div className="text-3xl font-bold underline text-green-500">
+            Hello
+            <button
+                className="flex justify-center mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-3/4 mx-auto"
+                onClick={onClickNextPage}
+            >
+                button
+            </button>
+        </div>
+    );
+};
+
+export default Home;
