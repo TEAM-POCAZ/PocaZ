ff85626026b1c4d8152e332b92d58b64c0b0e9ac
zoo0190 <66614341+zoo0190@users.noreply.github.com>
2022-09-09 16:20:51 +0900

    prettier, eslint 셋팅, package.json script 추가

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,17 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import helemt from 'helmet';
+
 const app = express();
 const port = 8000;
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(helemt());
+
+// TODO -  cors 다 열어놓으면 안됨 나중에 수정필요!
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello Pocaz');
