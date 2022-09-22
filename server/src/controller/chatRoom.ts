cbc79103a2813fa5aaecda3f188cb9608715cc92
zoo0190 <66614341+zoo0190@users.noreply.github.com>
2022-09-22 18:17:31 +0900

    typescript query test

--- server/src/controller/chatRoom.ts
+++ server/src/controller/chatRoom.ts
@@ -1,9 +1,21 @@
+import { RowDataPacket } from "mysql2"
 import { Request, Response, NextFunction } from 'express';
+import { db } from '../db/database';
 
-export const searchChatRoom = (
+interface Test extends RowDataPacket {
+  id: number;
+  name: string;
+}
+
+export const searchChatRoom = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  res.send('chatroom!');
+  const test = await db
+    .execute<Test[]>('SELECT * FROM Test')
+    .then((result) => result[0]);
+
+  console.log(test);
+  res.status(200).json(test);
 };
