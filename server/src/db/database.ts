ec9cc68b096e2c95a5d0aba8994caed95eaf48cc
zoo0190 <66614341+zoo0190@users.noreply.github.com>
2022-09-23 20:34:56 +0900

    sql handler

--- server/src/db/database.ts
+++ server/src/db/database.ts
@@ -1,13 +1,26 @@
-import mysql from 'mysql2';
+import mysql, { Pool } from 'mysql2/promise';
 import { config } from '../config';
 
 const { host, user, database, password } = config.db;
 
-const pool = mysql.createPool({
-  host,
-  user,
-  database,
-  password,
-});
+const db = (() => {
+  let dbPool: Pool;
+  const initiate = () => {
+    return mysql.createPool({
+      host,
+      user,
+      database,
+      password,
+    });
+  };
+  return {
+    getPool: function () {
+      if (!dbPool) {
+        dbPool = initiate();
+        return dbPool;
+      } else return dbPool;
+    },
+  };
+})();
 
-export const db = pool.promise();
+export default db;
