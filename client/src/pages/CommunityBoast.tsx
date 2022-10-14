191eb5ff862783c843660bf26967816c88aea24a
Byeolikim <213069@naver.com>
2022-10-14 20:14:45 +0900

    feat: 커뮤니티 자랑 카테고리 기초 세팅

--- client/src/pages/CommunityBoast.tsx
+++ client/src/pages/CommunityBoast.tsx
@@ -1,12 +1,50 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
 import Layout from 'utils/Layout'
+import CommunityTop from './CommunityTop'
+import axios from 'axios'
 
 const CommunityBoast = () => {
+  const [list, setList] = useState<any[] | null>(null)
+  useEffect(() => {
+    const list = async () => {
+      try {
+        setList(null)
+        const response = await axios.get('https://pocaz.ystoy.shop/api/post/2')
+        setList(response.data)
+      } catch (e) {
+        console.error(e)
+      }
+    }
+    list()
+  }, [])
   return (
     <>
       <Layout>
-        <div className="text-3xl font-bold underline text-blue-500">자랑 게시판임요</div>
+        <CommunityTop />
+        <div className="boastListWrap">
+          <ul>
+            {list &&
+              list.map((lists: any) => (
+                <li key={lists.id}>
+                  <div className="boastThumb">
+                    <img src={lists.filePath} />
+                  </div>
+                  <div className="boardSubject">
+                    <p>{lists.title}</p>
+                    <div>
+                      <div className="writeWrap">
+                        <div className="writeProfile">{/* <img src={} /> */}</div>
+                        <span className="writeName">{lists.nickname}</span>
+                      </div>
+                      <time>{lists.createAt}</time>
+                      <span className="comment">댓글 {lists.replyCnt}</span>
+                      <span className="hit">{lists.viewCount}</span>
+                    </div>
+                  </div>
+                </li>
+              ))}
+          </ul>
+        </div>
       </Layout>
     </>
   )
