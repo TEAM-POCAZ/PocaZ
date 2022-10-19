09f5a71901ff41dd4ddcd5e54c605d3cf834baad
Byeolikim <213069@naver.com>
2022-10-19 19:49:12 +0900

    style: font-size 수정

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -9,10 +9,12 @@ const Footer = () => {
           <Link to="/">POCAZ.</Link>
         </h6>
       </div>
-      <p className="text-zinc-500 text-sm">
+      <p className="text-zinc-500 text-xs tracking-tighter">
         서울특별시 영등포구 선유로9길 30 106동 청년취업사관학교 영등포캠퍼스
       </p>
-      <p className="text-zinc-500 text-sm">Copyright by POCAZ. All rights reserved.</p>
+      <p className="text-zinc-500 text-xs tracking-tighter">
+        Copyright by POCAZ. All rights reserved.
+      </p>
       <a className="pr-1 text-zinc-600 text-xs" href="#notion1" target="_blank">
         개인정보처리방침 |
       </a>
