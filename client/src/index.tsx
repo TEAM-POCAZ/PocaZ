05320cbc22a3ce09aa4aeb8c3413ec6f1fc0bacd
ho2yahh <ho2yahh@gmail.com>
2022-10-26 17:56:45 +0900

    chatroom join issue

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App'
 import Socket from './network/socket'
 
-const baseURL = process.env.REACT_APP_HOST_URL
+const baseURL: any = process.env.REACT_APP_HOST_URL
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 const socketTest = new Socket(baseURL as string)
