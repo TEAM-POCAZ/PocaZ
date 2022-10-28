b3bfe523dbb448ce9373d7a3aa9349c018fcaf04
Ring-wdr <akswnd55@gmail.com>
2022-10-29 02:19:34 +0900

    fix: 자랑게시판에서 글 작성 시 자랑게시판에 바로 작성하도록 변경

--- client/src/pages/CommunityBoast.jsx
+++ client/src/pages/CommunityBoast.jsx
@@ -1,36 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import Layout from '../utils/Layout'
-import { useNavigate } from 'react-router-dom'
-import CommunityTop from './CommunityTop'
-import axios from 'axios'
-import SearchBox from '../components/Square/SearchBox'
-import dayjs from 'dayjs'
+import React, { useState, useEffect } from "react";
+import Layout from "../utils/Layout";
+import { useNavigate } from "react-router-dom";
+import CommunityTop from "./CommunityTop";
+import axios from "axios";
+import SearchBox from "../components/Square/SearchBox";
+import dayjs from "dayjs";
 
 const CommunityBoast = () => {
-  const [list, setList] = useState(null)
-  const navigate = useNavigate()
+  const [list, setList] = useState(null);
+  const navigate = useNavigate();
   useEffect(() => {
     const boastList = async () => {
       try {
-        setList(null)
-        const response = await axios.get('http://localhost:8080/api/post/2')
-        setList(response.data)
+        setList(null);
+        const response = await axios.get("http://localhost:8080/api/post/2");
+        setList(response.data);
       } catch (e) {
-        console.error(e)
+        console.error(e);
       }
-    }
-    boastList()
-  }, [])
+    };
+    boastList();
+  }, []);
   return (
     <>
       <Layout>
         <SearchBox />
-        <CommunityTop />
+        <CommunityTop category={2} />
         <div className="boastListWrap">
           <ul>
             {list &&
               list.map((lists) => {
-                const days = dayjs(lists.createAt).format('YYYY-MM-DD')
+                const days = dayjs(lists.createAt).format("YYYY-MM-DD");
                 return (
                   <li
                     key={lists.id}
@@ -44,22 +44,29 @@ const CommunityBoast = () => {
                       <p>{lists.title}</p>
                       <div>
                         <div className="writeWrap">
-                          <div className="writeProfile">{/* <img src={} /> */}</div>
+                          <div className="writeProfile">
+                            {/* <img src={} /> */}
+                          </div>
                           <span className="writeName">{lists.nickname}</span>
                         </div>
                         <time className="text-xs">{days}</time>&nbsp;
-                        <span className="comment text-xs">댓글 {lists.replyCnt}</span>&nbsp;
-                        <span className="hit text-xs">조회 {lists.viewCount}</span>
+                        <span className="comment text-xs">
+                          댓글 {lists.replyCnt}
+                        </span>
+                        &nbsp;
+                        <span className="hit text-xs">
+                          조회 {lists.viewCount}
+                        </span>
                       </div>
                     </div>
                   </li>
-                )
+                );
               })}
           </ul>
         </div>
       </Layout>
     </>
-  )
-}
+  );
+};
 
-export default CommunityBoast
+export default CommunityBoast;
