a7a9d968bdfcd224dc52ea5975e0b0078ce42a07
Byeolikim <213069@naver.com>
2022-10-28 15:13:00 +0900

    feat: 장터 상단 카테고리

--- client/src/components/Market/MarketCategory.jsx
+++ client/src/components/Market/MarketCategory.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from "react";
 
+const IdolCateList = [
+  { id: 0, title: "더보이즈" },
+  { id: 1, title: "엔시티" },
+  { id: 2, title: "뉴진스" },
+  { id: 3, title: "에스파" },
+  { id: 4, title: "아이브" },
+  { id: 5, title: "르세라핌" },
+  { id: 6, title: "블랙핑크" },
+];
 const MarketCategory = () => {
   return (
     <>
-      <h2>장터 카테고리 컴포넌트</h2>
+      <div className="bg-white h-7">
+        <ul className="flex items-center justify-around h-full">
+          {IdolCateList.map((cate) => {
+            return (
+              <li key={cate.id}>
+                <p className="text-xs">{cate.title}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
-  )
-}
-export default MarketCategory
+  );
+};
+export default MarketCategory;
