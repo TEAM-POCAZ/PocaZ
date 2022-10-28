709173038c87e2b74136ea8a43ecfa3fc7b90226
ho2yahh <ho2yahh@gmail.com>
2022-10-28 11:59:54 +0900

    feat : 채팅 기능 전반 수정 with login

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -25,78 +25,81 @@ import InputMsg from "./InputMsg";
  */
 
 const Chat = ({ socket }) => {
-  const [chats, setChats] = useState([]);
-  const { userInfo, setNewMsg } = useStore();
+    const {
+        userInfo: { nickname: name, id },
+    } = useStore();
+    const [chats, setChats] = useState([]);
 
-  const navigate = useNavigate();
-  const location = useLocation();
-  const { room, name } = queryString.parse(location.search);
+    const navigate = useNavigate();
+    const location = useLocation();
+    // const { room } = queryString.parse(location.search);
 
-  // const { oppNickname }: any = location.state //TODO 지워야함
+    const { oppNickname, room } = location.state; //TODO 지워야함
 
-  useEffect(() => {
-    socket.joinRoom(room); //TODO login 붙으면 조인 빼야됨
+    useEffect(() => {
+        socket.joinRoom(room); //TODO login 붙으면 조인 빼야됨
 
-    socket.onSync("test", (message) => {
-      setChats((prev) => [...prev, message]);
-      setNewMsg(message);
-      //TODO 이 a 값을 zustand에도 넣는다.
-      // 이 값을 chatlist도 바라보게한다.
-    });
+        socket.onSync("test", (message) => {
+            setChats((prev) => [...prev, message]);
+            //TODO 이 a 값을 zustand에도 넣는다.
+            // 이 값을 chatlist도 바라보게한다.
+        });
 
-    // clear up function 이라고 하며 unmount 시 실행됨
-    // return () => close() // useEffect 동작 전 실행됨
-  }, []);
+        // clear up function 이라고 하며 unmount 시 실행됨
+        // return () => close() // useEffect 동작 전 실행됨
+    }, []);
 
-  const getChat = async () => {
-    if (room && name) {
-      const { data } = await apis.getChat(room);
-      console.log("data :>> ", data);
-      setChats(data);
-      return data;
-    }
-  };
-  const chatss = useMemo(() => {
-    getChat();
-  }, []);
+    const getChat = async () => {
+        if (room && name) {
+            const { data } = await apis.getChat(room);
+            setChats(data);
+            return data;
+        }
+    };
+    const chatss = useMemo(() => {
+        getChat();
+    }, []);
 
-  // const { isLoading, error, data } = useQuery<IChat[], Error>('getChat', getChat, {
-  //   // refetchOnWindowFocus: false,
-  // })
+    // const { isLoading, error, data } = useQuery<IChat[], Error>('getChat', getChat, {
+    //   // refetchOnWindowFocus: false,
+    // })
 
-  const handleMessage = async (sendMessage) => {
-    if (sendMessage) {
-      console.log("name, sendM :>> ", name, sendMessage, room);
-      const newMessage = {
-        user: userInfo.id,
-        message: sendMessage,
-        chatRoom: room,
-      };
+    const handleMessage = async (sendMessage) => {
+        if (sendMessage) {
+            console.log("name, sendM :>> ", name, sendMessage, room);
+            const newMessage = {
+                user: id,
+                message: sendMessage,
+                chatRoom: room,
+            };
 
-      socket.emitSync("message", newMessage);
-      // socket.emitSync('sendMessage', newMessage)
-      // await apis.postChat(newMessage) // post로 보낼 때
-      // const { data } = await apis.postChat(newMessage)
-      // setChats((prev) => [...prev, data])
-    }
-  };
+            socket.emitSync("message", newMessage);
+            // socket.emitSync('sendMessage', newMessage)
+            // await apis.postChat(newMessage) // post로 보낼 때
+            // const { data } = await apis.postChat(newMessage)
+            // setChats((prev) => [...prev, data])
+        }
+    };
 
-  const isLoading = false;
-  return (
-    <Layout>
-      {isLoading ? (
-        <>로딩중입니다</>
-      ) : (
-        <div className="flex items-center justify-center bg-gray-800 outerContainer h-[100vh]">
-          <div className="flex flex-col justify-between w-full bg-white rounded-lg h-2/3">
-            <InfoBar navigate={navigate} />
-            {/* <InfoBar oppNickname={oppNickname} navigate={navigate} /> */}
-            <Messages chats={chats} />
-            <InputMsg handleMessage={handleMessage} />
-          </div>
-        </div>
-      )}
-    </Layout>
-  );
+    const isLoading = false;
+    return (
+        <Layout>
+            {isLoading ? (
+                <>로딩중입니다</>
+            ) : (
+                <div className="flex items-center justify-center bg-gray-800 outerContainer h-[100vh]">
+                    <div className="flex flex-col justify-between w-full bg-white rounded-lg h-2/3">
+                        {/* <InfoBar navigate={navigate} /> */}
+                        <InfoBar
+                            oppNickname={oppNickname}
+                            navigate={navigate}
+                        />
+                        <Messages chats={chats} />
+                        <InputMsg handleMessage={handleMessage} />
+                    </div>
+                </div>
+            )}
+        </Layout>
+    );
 };
 export default Chat;
