ed567540a4702706ffa30046d54f988e98f4014e
Byeolikim <213069@naver.com>
2022-11-04 17:17:51 +0900

    style: 장터 디테일 수정

--- client/src/pages/MarketDetail.jsx
+++ client/src/pages/MarketDetail.jsx
@@ -8,16 +8,16 @@ import axios from "axios";
 const MarketDetail = () => {
   const navigate = useNavigate();
   const [content, setContent] = useState([]);
-  const {id} = useParams();
+  const { id } = useParams();
 
-  useEffect(()=>{
-    (async () =>{
-      const {data:[result]} = await axios.get(
-        `http://localhost:8080/api/market/${id}`
-      );
+  useEffect(() => {
+    (async () => {
+      const {
+        data: [result],
+      } = await axios.get(`http://localhost:8080/api/market/${id}`);
       setContent(result);
     })();
-  },[])
+  }, []);
 
   return (
     <>
@@ -33,30 +33,31 @@ const MarketDetail = () => {
             <i className="ri-chat-3-fill text-2xl"></i>
           </button>
         </div>
-        {
-          content &&
-        <div className="m-2.5">
-          {/* <div className="pocaDetailImg h-80 mb-2.5 pb-80 rounded-xl bg-slate-200"> */}
-            <img className="relative h-80 mb-2.5 rounded-xl" src={content.pocaImg} alt={content.pocaName}/>
-          {/* </div> */}
-          <div className="pocaDesc pt-2">
-            <h4 className="flex mb-1 text-sm">
-              <span className="pr-2">{content.groupName}</span>
-              <span className="text-slate-400">{content.stageName}</span>
-            </h4>
-            <h4 className="pb-2 font-semibold">
-              {content.title}
-            </h4>
-            <p className="text-gray-400 text-sm break-all">
-              {content.sellDesc}
-            </p>
-            <p className="my-2.5 text-2xl font-bold">
-              {content.price}
-              <b className="font-normal">원</b>
-            </p>
+        {content && (
+          <div className="m-2.5">
+            {/* <div className="pocaDetailImg h-80 mb-2.5 pb-80 rounded-xl bg-slate-200"> */}
+            <img
+              className="relative w-full h-full object-cover mb-2.5 rounded-xl"
+              src={content.pocaImg}
+              alt={content.pocaName}
+            />
+            {/* </div> */}
+            <div className="pocaDesc pt-2">
+              <h4 className="flex mb-1 text-sm">
+                <span className="pr-2">{content.groupName}</span>
+                <span className="text-slate-400">{content.stageName}</span>
+              </h4>
+              <h4 className="pb-2 font-semibold">{content.title}</h4>
+              <p className="text-gray-400 text-sm break-all">
+                {content.sellDesc}
+              </p>
+              <p className="my-2.5 text-2xl font-bold">
+                {content.price}
+                <b className="font-normal">원</b>
+              </p>
+            </div>
           </div>
-        </div> 
-        }
+        )}
       </Layout>
     </>
   );
