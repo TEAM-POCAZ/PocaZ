fea41462baf26a6bb5126393d28f5406918e06b2
Byeolikim <213069@naver.com>
2022-11-04 17:05:49 +0900

    feat: 장터 목록 무한 스크롤

--- client/src/components/Market/MarketListItem.jsx
+++ client/src/components/Market/MarketListItem.jsx
@@ -2,43 +2,49 @@ import React from "react";
 import axios from "axios";
 import { useQuery } from "react-query";
 
-const MarketListItem = () => {
-  const result = useQuery("pocas", () =>
-    axios.get("http://localhost:8080/api/market").then((a) => {
-      return a.data.sellList;
-    })
-  );
+const MarketListItem = ({ list }) => {
+  // const result = useQuery("pocas", () =>
+  //   axios.get("http://localhost:8080/api/market").then((a) => {
+  //     return a.data.sellList;
+  //   })
+  // );
+
   return (
     <>
       <div className="mx-3.5">
         <ul className="flex flex-row flex-wrap justify-between">
-          {result.data &&
-            result.data.map((poca) => (
-              <li className="flex-[0_1_48%] mb-3.5" key={poca.id}>
-                <div className="pocaThumb relative h-72 lg:h-96 mm:h-60 rounded-xl overflow-hidden">
-                  <img
-                    src={poca.pocaImg}
-                    className="w-full h-full object-cover"
-                  />
-                </div>
-                <div className="pocaListBox mt-2.5 p-3.5 rounded-xl bg-white text-xs box-border">
-                  <p className="groupName font-extrabold text-sm text-[#034ac5]">
-                    {poca.groupName}✨
-                  </p>
-                  <p className="memberName text-sm">{poca.stageName}</p>
-                  <p className="pocaDetail mt-2.5 font-medium text-base">
-                    {poca.pocaName}
-                  </p>
-                  <p className="pocaDesc mb-1 text-gray-500">
-                    {poca.description}
-                  </p>
-                  <p className="pocaPrice mt-2.5 font-semibold text-base">
-                    <span>{poca.price}</span>
-                    <span className="won">원</span>
-                  </p>
-                </div>
-              </li>
-            ))}
+          {list?.pages?.map((page) => (
+            <React.Fragment key={page.nextId}>
+              {page.sellList.map((post) => {
+                return (
+                  <li className="flex-[0_1_48%] mb-3.5" key={post.id}>
+                    <div className="pocaThumb relative h-72 lg:h-96 mm:h-60 rounded-xl overflow-hidden">
+                      <img
+                        src={post.pocaImg}
+                        className="w-full h-full object-cover"
+                      />
+                    </div>
+                    <div className="pocaListBox mt-2.5 p-3.5 rounded-xl bg-white text-xs box-border">
+                      <p className="groupName font-extrabold text-sm text-[#034ac5]">
+                        {post.groupName}✨
+                      </p>
+                      <p className="memberName text-sm">{post.stageName}</p>
+                      <p className="pocaDetail mt-2.5 font-medium text-base">
+                        {post.pocaName}
+                      </p>
+                      <p className="pocaDesc mb-1 text-gray-500">
+                        {post.description}
+                      </p>
+                      <p className="pocaPrice mt-2.5 font-semibold text-base">
+                        <span>{post.price}</span>
+                        <span className="won">원</span>
+                      </p>
+                    </div>
+                  </li>
+                );
+              })}
+            </React.Fragment>
+          ))}
         </ul>
       </div>
     </>
--- client/src/pages/MarketList.jsx
+++ client/src/pages/MarketList.jsx
@@ -1,16 +1,76 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../utils/Layout";
 import MarketSearchBox from "../components/Market/MarketSearchBox";
 import MarketCategory from "../components/Market/MarketCategory";
 import MarketListItem from "../components/Market/MarketListItem";
+import { useInView } from "react-intersection-observer";
+import { useInfiniteQuery } from "react-query";
+import axios from "axios";
+
+const NUMBER_OF_POSTS_ON_PAGE = 10;
 
 const MarketList = () => {
+  const { ref, inView } = useInView();
+
+  const {
+    status,
+    data,
+    error,
+    isFetching,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery(
+    ["projects"],
+    async ({ pageParam = Number.MAX_SAFE_INTEGER }) => {
+      const res = await axios.get(
+        `http://localhost:8080/api/market?lastPostId=${pageParam}&SIZE=${NUMBER_OF_POSTS_ON_PAGE}`
+      );
+      return res.data;
+    },
+    {
+      getPreviousPageParam: (firstPage) => firstPage.previousId ?? undefined,
+      getNextPageParam: (lastPage) => lastPage.nextId ?? undefined,
+    }
+  );
+  useEffect(() => {
+    inView && fetchNextPage();
+  }, [inView]);
+
   return (
     <>
       <Layout>
         <MarketSearchBox />
         <MarketCategory />
-        <MarketListItem />
+
+        {status === "loading" ? (
+          <p>Loading...</p>
+        ) : status === "error" ? (
+          <span>Error: {error.message}</span>
+        ) : (
+          <>
+            <MarketListItem list={data} />
+
+            <div>
+              <button
+                ref={ref}
+                onClick={() => fetchNextPage()}
+                disabled={!hasNextPage || isFetchingNextPage}
+              >
+                {isFetchingNextPage
+                  ? "Loading more..."
+                  : hasNextPage
+                  ? "Load Newer"
+                  : "Nothing more to load"}
+              </button>
+            </div>
+            <div>
+              {isFetching && !isFetchingNextPage
+                ? "Background Updating..."
+                : null}
+            </div>
+          </>
+        )}
       </Layout>
     </>
   );
