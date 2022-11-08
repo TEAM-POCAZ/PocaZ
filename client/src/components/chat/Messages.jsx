d4b43fc801dba143a36160db848c7ccea4ade2d0
ho2yahh <ho2yahh@gmail.com>
2022-11-09 01:56:51 +0900

    feat : Chat scroll-to-bottom 제거 및 UI 변경

--- client/src/components/chat/Messages.jsx
+++ client/src/components/chat/Messages.jsx
@@ -1,20 +1,31 @@
-import React from "react";
-import ScrollToBottom from "react-scroll-to-bottom";
+import React, { useRef, useState } from "react";
+import { useEffect } from "react";
 
 import Msg from "./Msg";
 
-const Messages = ({ chats, oppNickname }) => {
-  return (
-    <ScrollToBottom className="flex-auto px-4 overflow-auto bg-gray-100 ">
-      {chats.map((chat, i) => {
-        return (
-          <div key={i}>
-            <Msg chat={chat} oppNickname={oppNickname} />
-          </div>
-        );
-      })}
-    </ScrollToBottom>
-  );
+const Messages = ({ chats, sellerNickname }) => {
+    const [isSmooth, setIsSmooth] = useState("auto");
+    const scrollRef = useRef();
+
+    useEffect(() => {
+        scrollRef.current?.scrollIntoView({ behavior: isSmooth });
+        return () => setIsSmooth("smooth");
+    }, [chats]);
+
+    return (
+        <div className="flex-auto px-4 overflow-auto scrollbar-hide h-full bg-gray-100">
+            {chats.map((chat) => {
+                return (
+                    <React.Fragment key={chat.id}>
+                        <div>
+                            <Msg chat={chat} sellerNickname={sellerNickname} />
+                        </div>
+                        <div ref={scrollRef} />
+                    </React.Fragment>
+                );
+            })}
+        </div>
+    );
 };
 
 export default Messages;
