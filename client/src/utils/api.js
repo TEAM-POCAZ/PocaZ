7fb07186aa1008abd5e4fab45f1fadef347c6618
Ring-wdr <akswnd55@gmail.com>
2022-11-14 01:43:46 +0900

    feat: 장터 이미지 가져오는 요청 추가

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,11 +1,11 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: 'http://localhost:8080/api',
   // baseURL: 'https://slowtest.ml/api',
   headers: {
-    "Content-type": "application/json; charset=UTF-8",
-    accept: "application/json,",
+    'Content-type': 'application/json; charset=UTF-8',
+    accept: 'application/json,',
   },
 });
 export const source = axios.CancelToken.source();
@@ -17,12 +17,14 @@ export const apis = {
   getChat: (roomNumber) => api.get(`/chat/${roomNumber}`), // Chat.tsx
   getSellItem: (marketItemId) => api.get(`/chat/item/${marketItemId}`),
   getCheckChatRoom: (marketItemId, loginUserId) => {
-    api.get(`/chat/CheckChatRoom?marketItemId=${marketItemId}&loginUserId=${loginUserId}`)
+    api.get(
+      `/chat/CheckChatRoom?marketItemId=${marketItemId}&loginUserId=${loginUserId}`
+    );
   },
   getMarketDetail: (id) => api.get(`/market/${id}`), // MarketDetail.tsx
-
+  getMarketImgs: (id) => api.get(`/market/img/${id}`), // use in Detail and write
   // post
-  postChat: (data) => api.post("/chat", data), //update 시킬 함수 추가
+  postChat: (data) => api.post('/chat', data), //update 시킬 함수 추가
 
   // market
   MarketSearch: (data) => {
