46a0884688c6ce997a2dd3bdf7cdcdfbdc888ad4
Ring-wdr <akswnd55@gmail.com>
2022-11-14 20:05:25 +0900

    fix: DB의 카테고리를 가져오도록 수정

--- client/src/components/Market/MarketCategory.jsx
+++ client/src/components/Market/MarketCategory.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { apis } from '../../utils/api';
 
-const IdolCateList = [
-  { id: 0, title: '전체' },
-  { id: 1, title: '에스파' },
-  { id: 2, title: '소녀시대' },
-  { id: 3, title: '뉴진스' },
-];
+const getIdolList = async (setIdol) => {
+  const { data: IdolCateList } = await apis.getArtistGroup();
+  setIdol(IdolCateList);
+};
+
 const MarketCategory = ({ setGroup }) => {
+  const [IdolList, setIdol] = useState([]);
+
+  useEffect(() => {
+    getIdolList(setIdol);
+  }, []);
+
   return (
     <>
       <div className='bg-white h-7 mb-1'>
         <ul className='flex items-center justify-around h-full'>
-          {IdolCateList.map((cate) => {
+          <li key={0} onClick={() => setGroup(0)}>
+            <p className='text-xs cursor-pointer text-slate-600 hover:text-blue-700'>
+              전체
+            </p>
+          </li>
+          {IdolList.map((cate) => {
             return (
               <li
                 key={cate.id}
@@ -19,8 +32,9 @@ const MarketCategory = ({ setGroup }) => {
                   setGroup(cate.id);
                 }}
               >
+                <img src={cate.grouplogoUrl} className='rounded-full'></img>
                 <p className='text-xs cursor-pointer text-slate-600 hover:text-blue-700'>
-                  {cate.title}
+                  {cate.koreanName}
                 </p>
               </li>
             );
