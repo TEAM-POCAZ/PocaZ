59e80aab5f41eab1dd543c4f8a2a031149d9cb3a
Ubuntu <ubuntu@ip-172-31-35-167.ap-northeast-2.compute.internal>
2022-11-17 07:22:55 +0000

    목록 나오게 수정

--- client/src/components/MyPage/Artists.jsx
+++ client/src/components/MyPage/Artists.jsx
@@ -81,7 +81,7 @@ const Artists = ({ setArtistId }) => {
             >
               <div className='pocaThumb relative h-72 lg:h-96 mm:h-60 rounded-xl overflow-hidden'>
                 <img
-                  src={artist.pocaImg}
+                  src={imgList[artist.id - 1]}
                   className='w-full h-full object-cover'
                 />
               </div>
@@ -100,3 +100,44 @@ const Artists = ({ setArtistId }) => {
 };
 
 export default Artists;
+
+const imgList = [
+  'https://media.discordapp.net/attachments/1039076920492560445/1039083845749833758/opal_.jpeg?width=392&height=625',
+  'https://media.discordapp.net/attachments/1039076920492560445/1039083845368164473/onyx_.jpeg?width=392&height=624',
+  'https://media.discordapp.net/attachments/1039076920492560445/1039083845183602708/3a7400014db9a07e.jpeg?width=410&height=625',
+  'https://media.discordapp.net/attachments/1039076920492560445/1039096240689922078/unknown.png?width=404&height=625',
+  'https://media.discordapp.net/attachments/1039076920492560445/1039095965136728074/unknown.png?width=396&height=625',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1039444731748417536/78fc132866859579.jpeg',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1039446046343974962/MAVERICK_ID_CARD.jpeg',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1039114044776398849/5d48b14015d6f01c.jpeg',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1039109178003828766/DMC.jpeg',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1039116123939020840/MAVERICK_ID_CARD.jpeg',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1039449683053006870/b8cd2436421d8f75.jpeg',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1039436843088891914/a232d8ad2cdea6ef.jpeg',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1039426187157590026/EWalceTU8AACh4A.jpeg',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1039441877801385994/cb9f89e447a2d135.jpeg',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1039429072033419294/42.jpeg',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1039450384395145276/03bd3bac9b38bf26.jpeg',
+  'https://media.discordapp.net/attachments/1039076920492560445/1039205605967143015/B_.jpg?width=389&height=583',
+  'https://media.discordapp.net/attachments/1039076920492560445/1039205604767576165/B_4.jpg?width=389&height=583',
+  'https://media.discordapp.net/attachments/1039076920492560445/1039205605728063599/B_.jpg?width=389&height=583',
+  'https://media.discordapp.net/attachments/1039076920492560445/1039205603500888074/B_2.jpg?width=389&height=583',
+  'https://media.discordapp.net/attachments/1039076920492560445/1039205604071313469/B_2.jpg?width=389&height=583',
+  'https://media.discordapp.net/attachments/1039076920492560445/1039443766328688670/Karina-SSGT2022-AppleMusic.jpg?width=364&height=605',
+  'https://media.discordapp.net/attachments/1039076920492560445/1039477328671154266/Giselle-savage_p.o.s_ver-ar_clip_cardjpg.jpg?width=404&height=606',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1039450056702570559/Winter-savage__Photocard_.jpg',
+  'https://media.discordapp.net/attachments/1039076920492560445/1039471425985191946/Zizel-Girls_Mumo.jpg?width=385&height=606',
+  'https://media.discordapp.net/attachments/1039076920492560445/1042698675471798352/unknown.png',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1042698341751988244/DpnK3xIV4AA_REP.jpg',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1040164437090369586/313f4dc2cb1e1525.jpeg',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1040166033220173864/5db3310ba5d868db.jpeg',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1041153732391473193/nctpt1.jpg',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1041154864429928499/nctpt2.jpg',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1041155082475032677/nctpt2.jpg',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1041156059542335608/nctarri.jpg',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1041154337377878086/nctpt2.jpg',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1041149672183582800/nctdream.jpg',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1041153990622183484/nctpt1.jpg',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1041152769177952326/nctRESONANCE.jpg',
+  'https://cdn.discordapp.com/attachments/1039076920492560445/1041153369143791697/nctpt1.jpg',
+];
