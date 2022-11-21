255e08e7520fae3d4bbe53006827676317d54c7c
Byeolikim <213069@naver.com>
2022-11-21 23:27:53 +0900

    style: 장터, 메인 깨지는 문제 다시 해결

--- client/src/components/Market/MarketListItem.jsx
+++ client/src/components/Market/MarketListItem.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import axios from 'axios';
-import { useQuery } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 import { baseURL } from '../../utils/api';
 
@@ -19,38 +17,43 @@ const MarketListItem = ({ list }) => {
               {page.sellList.map((post) => {
                 return (
                   <li
-                    className='flex-[0_1_48%] mb-3.5 cursor-pointer'
+                    className='flex-[0_1_48%] w-[45%] mb-3.5 cursor-pointer'
                     key={post.id}
-                    onClick={() => {
-                      navigate(`/Market/${post.id}`);
-                    }}
                   >
-                    <div className='pocaThumb relative h-72 lg:h-96 mm:h-60 rounded-xl overflow-hidden'>
-                      <img
-                        src={`${baseURL}/${post.filePath}`}
-                        crossOrigin={'anonymous'}
-                        className='w-full h-full object-cover'
-                      />
-                    </div>
-                    <div className='pocaListBox mt-2.5 p-3.5 rounded-xl bg-white text-xs box-border'>
-                      <p className='groupName font-extrabold text-sm text-[#034ac5]'>
-                        {post.groupName}✨
-                      </p>
-                      <p className='memberName text-sm'>{post.stageName}</p>
-                      <p className='pocaDetail mt-1.5 font-medium text-sm text-gray-300 w-[180px] overflow-hidden whitespace-nowrap text-ellipsis'>
-                        {post.pocaName}
-                      </p>
-                      <p className='pocaDetail mt-2.5 font-medium text-lg'>
-                        {post.title}
-                      </p>
-                      {/* <p className='pocaDesc mb-1 text-gray-500'>
+                    <button
+                      className='w-full'
+                      onClick={() => {
+                        navigate(`/Market/${post.id}`);
+                      }}
+                    >
+                      <div className='pocaThumb relative h-72 lg:h-96 mm:h-60 rounded-xl overflow-hidden'>
+                        <img
+                          src={`${baseURL}/${post.filePath}`}
+                          crossOrigin={'anonymous'}
+                          className='w-full h-full object-cover'
+                          alt='포토카드 썸네일'
+                        />
+                      </div>
+                      <div className='pocaListBox mt-2.5 p-3.5 rounded-xl bg-white text-xs box-border text-left'>
+                        <p className='groupName font-extrabold text-sm text-[#034ac5]'>
+                          {post.groupName}✨
+                        </p>
+                        <p className='memberName text-sm'>{post.stageName}</p>
+                        <p className='pocaDetail w-full mt-1.5 font-medium text-sm text-gray-300 overflow-hidden whitespace-nowrap text-ellipsis'>
+                          {post.pocaName}
+                        </p>
+                        <p className='pocaDetail mt-2.5 font-medium text-lg'>
+                          {post.title}
+                        </p>
+                        {/* <p className='pocaDesc mb-1 text-gray-500'>
                         {post.description}
                       </p> */}
-                      <p className='pocaPrice mt-2.5 font-semibold text-base text-right'>
-                        <span>{post.price.toLocaleString()}</span>
-                        <span className='won'>원</span>
-                      </p>
-                    </div>
+                        <p className='pocaPrice mt-2.5 font-semibold text-base text-right'>
+                          <span>{post.price.toLocaleString()}</span>
+                          <span className='won'>원</span>
+                        </p>
+                      </div>
+                    </button>
                   </li>
                 );
               })}
